31f66bb5457e9df44265a1a24068d127e2285729
Rachael <rachaellatimer@gmail.com>
2021-05-18 11:59:02 -0700

    start of proj script

---
+++ project_script.R
@@ -0,0 +1,15 @@
+#FP 653 Project Script
+
+library(tidyverse)
+library(janitor)
+library(ggplot2)
+library(here)
+library(rio)
+
+county_sch <- import(here("schools_county_csv.csv")) %>% 
+  clean_names() %>%
+  as_tibble()
+
+district_sch <- import(here("schools_district_csv.csv")) %>% 
+  clean_names() %>%
+  as_tibble()
