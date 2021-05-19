81b395ca46edcc4e64bf124bc9f70a8b2a44ae24
Rachael <rachaellatimer@gmail.com>
2021-05-19 01:26:13 -0700

    mapping data
    new data file with correct form of fips (all needed to be 5 digits). data mapped by county to US map

--- project_script.R
+++ project_script.R
@@ -5,15 +5,49 @@ library(janitor)
 library(ggplot2)
 library(here)
 library(rio)
+library(usmap)
+library(maps)
 
-county_sch <- import(here("schools_county_csv.csv")) %>% 
+#bring in data
+
+county_sch <- import(here("schools_county_csv2.xlsx")) %>% 
   clean_names() %>%
   as_tibble()
 
-district_sch <- import(here("schools_district_csv.csv")) %>% 
-  clean_names() %>%
-  as_tibble()
+#need the variable 'fips' for mapping
+cmap <- rename(county_sch, "fips" = "countyfips4")
 
-state_sch <- import(here("schools_state_csv.csv")) %>% 
-  clean_names() %>%
-  as_tibble()
+#only keep 2020 data
+
+#we may not need these next two data sets
+#district_sch <- import(here("schools_district_csv.csv")) %>% 
+#  clean_names() %>%
+#  as_tibble()
+
+#state_sch <- import(here("schools_state_csv.csv")) %>% 
+#  clean_names() %>%
+#  as_tibble()
+
+#function1
+
+
+#function2
+
+
+
+# us map with counties
+
+plot_usmap(regions = "counties", data = cmap, values = "share_all_closed_75") + 
+  labs(title = "title",
+       subtitle = "subtitle here") + 
+  theme(panel.background = element_rect(color = "black", fill = "lightblue"))
+
+#more formatting for map
+
+
+
+#function to output new map for each month?
+
+
+
+#shiny app to toggle between months of the 2020 year
